a510ecdf12864ee0efd54257483c946c2ab9e44c
mrvladis <mr.vladis@gmail.com>
2017-10-25 10:22:05 +0100

    fixes

--- Public/Get-MRVTempPassword.ps1
+++ Public/Get-MRVTempPassword.ps1
@@ -4,7 +4,9 @@ Function Get-MRVTempPassword
         [int]
         $length = 10
     )
-    $ascii = $NULL; For ($a = 33; $a –le 126; $a++) {$ascii += , [char][byte]$a }
+    $ascii = $NULL
+    For ($a = 33; $a –le 126; $a++)
+    {$ascii += , [char][byte]$a }
     For ($loop = 1; $loop –le $length; $loop++)
     {
         $TempPassword += ($ascii | GET-RANDOM)
--- Public/Start-MRVVMPowerAssesment.ps1
+++ Public/Start-MRVVMPowerAssesment.ps1
@@ -15,33 +15,33 @@ Function Start-MRVVMPowerAssesment
     $currentTime = [System.TimeZoneInfo]::ConvertTime($UTCTime, $oToTimeZone)
     $Message = ""
     Write-EventLog -LogName "Application" -Source "$EventAppName" -EventID 8001 -EntrySubscriptionName Information -Message "Runbook started at [$currentTime] for Subscription [$SubscriptionName]" -Category 1
-    Write-Verbose"Runbook started at [$currentTime] for Subscription [$SubscriptionName]"
+    Write-Verbose "Runbook started at [$currentTime] for Subscription [$SubscriptionName]"
     if ($Simulate)
     {
-        Write-Verbose"*** Running in SIMULATE mode. No power actions will be taken. ***"
+        Write-Verbose "*** Running in SIMULATE mode. No power actions will be taken. ***"
         Write-EventLog -LogName "Application" -Source "$EventAppName" -EventID 80010 -EntrySubscriptionName Warning -Message "*** Running in SIMULATE mode. No power actions will be taken. ***" -Category 1
     }
     else
     {
-        Write-Verbose"*** Running in LIVE mode. Schedules will be enforced. ***"
+        Write-Verbose "*** Running in LIVE mode. Schedules will be enforced. ***"
     }
-    Write-Verbose"Current time [$($currentTime.ToString("dddd, yyyy MMM dd HH:mm:ss"))] will be checked against schedules"
-    Write-Verbose"Logging in to $SubscriptionName"
+    Write-Verbose "Current time [$($currentTime.ToString("dddd, yyyy MMM dd HH:mm:ss"))] will be checked against schedules"
+    Write-Verbose "Logging in to $SubscriptionName"
     Select-MRVSubscription -SubscriptionName $SubscriptionName
 
     $Days = @("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    Write-Verbose"Getting VM"
+    Write-Verbose "Getting VM"
     $time = Get-Date
     $DayToday = (get-date).DayOfWeek
     $WeekOfMonth = [math]::Floor(((Get-Date).Day - 1) / 7 + 1)
     $Patching_Schedule = "23:00->03:00"
     $PatchingTagName = "Patching_Schedule"
-    Write-Verbose"Today is $DayToday and week number [$WeekOfMonth]"
-    Write-Verbose"Processing VMs. We have $($VMs.Count) to proceed"
+    Write-Verbose "Today is $DayToday and week number [$WeekOfMonth]"
+    Write-Verbose "Processing VMs. We have $($VMs.Count) to proceed"
     # Get a list of all virtual machines in subscription
     $VMList = @(Get-AzureRmResource | where {$_.ResourceSubscriptionName -like "Microsoft.*/virtualMachines"} | sort Name)
 
-    Write-Verbose"Processing [$($VMList.Count)] virtual machines found in subscription"
+    Write-Verbose "Processing [$($VMList.Count)] virtual machines found in subscription"
     ForEach ($VM in $VMList)
     {
         $Schedule = $null
@@ -49,7 +49,7 @@ Function Start-MRVVMPowerAssesment
         if ($tags -eq $null)
         {
             # No direct or inherited tag. Skip this VM.
-            Write-Verbose"[$($VM.Name)]: Does not have any tags for start / stop management. Skipping this VM."
+            Write-Verbose "[$($VM.Name)]: Does not have any tags for start / stop management. Skipping this VM."
             continue
         }
         $ifAlwaysOn = $false
@@ -57,26 +57,26 @@ Function Start-MRVVMPowerAssesment
         if ((($tags.GetEnumerator() | Where-Object {$_.Name -like "AlwaysON"}).Value).tolower() -like "*true")
         {
             $ifAlwaysOn = $true
-            Write-Verbose"[$($VM.Name)]: Has AlwaysOn set to TRUE ($ifAlwaysOn)"
+            Write-Verbose "[$($VM.Name)]: Has AlwaysOn set to TRUE ($ifAlwaysOn)"
         }
         else
         {
             #$ifAlwaysOn = $false # Already set above
-            Write-Verbose"[$($VM.Name)]: Has AlwaysOn set to False ($ifAlwaysOn)"
+            Write-Verbose "[$($VM.Name)]: Has AlwaysOn set to False ($ifAlwaysOn)"
         }
         if ((($tags.GetEnumerator() | Where-Object {$_.Name -like "AlwaysOFF"}).Value).tolower() -like "*true")
         {
             $ifAlwaysOff = $true
-            Write-Verbose"[$($VM.Name)]: Has AlwaysOFF set to TRUE ($ifAlwaysOff)"
+            Write-Verbose "[$($VM.Name)]: Has AlwaysOFF set to TRUE ($ifAlwaysOff)"
         }
         else
         {
             #$ifAlwaysOff = $false # Already set above
-            Write-Verbose"[$($VM.Name)]: Has AlwaysOn set to False ($ifAlwaysOff)"
+            Write-Verbose "[$($VM.Name)]: Has AlwaysOn set to False ($ifAlwaysOff)"
         }
         If (($ifAlwaysOn) -and ($ifAlwaysOff))
         {
-            Write-Verbose"[$($VM.Name)]: Has AlwaysOn and AlwaysOff both set to TRUE. This doesn't make sense. Skipping...."
+            Write-Verbose "[$($VM.Name)]: Has AlwaysOn and AlwaysOff both set to TRUE. This doesn't make sense. Skipping...."
             continue
         }
 
@@ -89,13 +89,13 @@ Function Start-MRVVMPowerAssesment
             catch
             {
                 # No direct or inherited tag. Skip this VM.
-                Write-Verbose"[$($VM.Name)]: Not tagged for the $DayToday. Skipping this VM."
+                Write-Verbose "[$($VM.Name)]: Not tagged for the $DayToday. Skipping this VM."
                 continue
             }
             if ($Schedule -eq $null)
             {
                 # No direct or inherited tag. Skip this VM.
-                Write-Verbose"[$($VM.Name)]: Not tagged for the $DayToday. Skipping this VM."
+                Write-Verbose "[$($VM.Name)]: Not tagged for the $DayToday. Skipping this VM."
                 continue
             }
             # Parse the ranges in the Tag value. Expects a string of comma-separated time ranges, or a single time range
@@ -124,50 +124,50 @@ Function Start-MRVVMPowerAssesment
             $IsScheduleMatched = $false
         }
         # After all checks - we are checking if the VM need to be UP for the maintenance. Even if it is alwaysOFF - it need to be kept up to date.
-        Write-Verbose"Checking if the VM need to be UP for the maintenance. Even if it is alwaysOFF - it need to be kept up to date."
+        Write-Verbose "Checking if the VM need to be UP for the maintenance. Even if it is alwaysOFF - it need to be kept up to date."
         if ($tags.ContainsKey($PatchingTagName))
         {
-            Write-Verbose"Patching Tag found. Checking if it is time for patching.."
+            Write-Verbose "Patching Tag found. Checking if it is time for patching.."
             $PatchingGroup = ($tags.GetEnumerator() | Where-Object {$_.Name -like $PatchingTagName}).Value
-            Write-Verbose"We have got Patching Group [$PatchingGroup]. Is it time for patching?"
+            Write-Verbose "We have got Patching Group [$PatchingGroup]. Is it time for patching?"
             $PatchingWeekNumber = $PatchingGroup.Substring(1, 1)
             $PatchingDay = $PatchingGroup.Substring(3, 3)
             if (($PatchingDay -eq $DayToday.ToString().substring(0, 3).ToUpper()) -or ($PatchingDay -eq (get-date).AddDays(-1).DayOfWeek.ToString().substring(0, 3).ToUpper()))
             {
-                Write-Verbose"It looks like currently Patching or After Patching Day. Checking the Time"
+                Write-Verbose "It looks like currently Patching or After Patching Day. Checking the Time"
                 if ((Test-MRVPowerScheduleEntry -TimeRange $Patching_Schedule -Patching))
                 {
-                    Write-Verbose"It looks like currently patching time, so we need to ensure that VM is up."
+                    Write-Verbose "It looks like currently patching time, so we need to ensure that VM is up."
                     $IsScheduleMatched = $true
                     $MatchedSchedule = $Patching_Schedule
                 }
             }
             else
             {
-                Write-Verbose"Not a Patching Day..."
+                Write-Verbose "Not a Patching Day..."
             }
         }
         else
         {
-            Write-Verbose"Patching Group Not Specified"
+            Write-Verbose "Patching Group Not Specified"
         }
         # Enforce desired state for group resources based on result.
         if ($IsScheduleMatched)
         {
             # Schedule is matched. Start the VM if it is not running.
-            Write-Verbose"[$($VM.Name)]: Current time [$currentTime] falls within VM running range [$MatchedSchedule]"
+            Write-Verbose "[$($VM.Name)]: Current time [$currentTime] falls within VM running range [$MatchedSchedule]"
             $DesiredState = "Started"
         }
         else
         {
             # Schedule not matched. Shut down VM if not stopped.
-            Write-Verbose"[$($VM.Name)]: Current time falls within of all scheduled shutdown ranges."
+            Write-Verbose "[$($VM.Name)]: Current time falls within of all scheduled shutdown ranges."
             $DesiredState = "StoppedDeallocated"
         }
         Start-Job Name $($VM.Name + '_' + $time) -ArgumentList $VM, $DesiredState, $Simulate, $AutomationCredentials, $SubscriptionName `
             Scriptblock {
             Param($VM, $DesiredState, $Simulate, $Connection, $SubscriptionName)
-            Write-Verbose"VM ID [$($VM.ResourceId)]"
+            Write-Verbose "VM ID [$($VM.ResourceId)]"
             Import-Module mrv_module
             Add-AzureRMAccount -ServicePrincipal -Tenant $Connection.TenantID -ApplicationID $Connection.ApplicationID -CertificateThumbprint $Connection.CertificateThumbprint
             Select-MRVSubscription -SubscriptionName $SubscriptionName
